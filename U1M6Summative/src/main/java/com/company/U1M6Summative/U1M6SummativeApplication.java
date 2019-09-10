237819046c63648ab830fb83c055fcd43c8ad3db
Ahmed Kaahin <ahmedkaahin@MohamedAliMacbookPro.local>
2019-09-10 14:42:54 -0400

    summative team project

---
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/U1M6SummativeApplication.java
@@ -0,0 +1,13 @@
+package com.company.U1M6Summative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U1M6SummativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U1M6SummativeApplication.class, args);
+	}
+
+}
---
+++ U1M6Summative/src/test/java/com/company/U1M6Summative/U1M6SummativeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.U1M6Summative;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class U1M6SummativeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

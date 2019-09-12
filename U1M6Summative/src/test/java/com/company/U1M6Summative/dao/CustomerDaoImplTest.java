739f6f9a32ac5a83ff0e85a1113df3d31565288b
JayMedina <jay.medina813@gmail.com>
2019-09-12 11:29:22 -0400

    initial commit for project without tests

---
+++ U1M6Summative/src/test/java/com/company/U1M6Summative/dao/CustomerDaoImplTest.java
@@ -0,0 +1,38 @@
+package com.company.U1M6Summative.dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomerDaoImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void saveCustomer() {
+    }
+
+    @Test
+    public void findCustomer() {
+    }
+
+    @Test
+    public void findAllCustomer() {
+    }
+
+    @Test
+    public void updateCustomer() {
+    }
+
+    @Test
+    public void deleteCustomer() {
+    }
+}

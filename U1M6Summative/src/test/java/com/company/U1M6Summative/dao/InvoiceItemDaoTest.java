581b9d623e86aef31039ded10c515124d71459c9
Asteria <dcdion22@gmail.com>
2019-09-12 13:21:26 -0400

    Pulling newest merge with updated invoice

--- U1M6Summative/src/test/java/com/company/U1M6Summative/dao/InvoiceItemDaoTest.java
+++ U1M6Summative/src/test/java/com/company/U1M6Summative/dao/InvoiceItemDaoTest.java
@@ -1,5 +1,6 @@
 package com.company.U1M6Summative.dao;
 
+import com.company.U1M6Summative.dto.Customer;
 import com.company.U1M6Summative.dto.Invoice;
 import com.company.U1M6Summative.dto.InvoiceItem;
 import com.company.U1M6Summative.dto.Item;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -24,6 +27,8 @@ public class InvoiceItemDaoTest {
     private InvoiceDao invoiceDao;
     @Autowired
     private ItemDao itemDao;
+    @Autowired
+    private CustomerDao customerDao;
 
     @Before
     public void setUp() throws Exception {
@@ -41,11 +46,42 @@ public class InvoiceItemDaoTest {
         for (Item item: itemList) {
             itemDao.deleteItem(item.getItemId());
         }
+
+        List<Customer> customerList = customerDao.findAllCustomer();
+        for (Customer c: customerList) {
+            customerDao.deleteCustomer(c.getCustomerId());
+        }
     }
+
+    /*
+    invoice_item_id int(11) not null auto_increment primary key,
+    invoice_id int(11) not null,
+    item_id int(11) not null,
+    quantity int(11) not null,
+    unit_rate decimal(8,2) not null,
+    discount decimal(8,2) not null
+     */
 
     @Test
     public void addGetDeleteInvoiceItems() {
-        
+        Customer customer = new Customer();
+        customer.setFirstName("Delcie");
+        customer.setLastName("Dion");
+        customer.setEmail("email@address.com");
+        customer.setCompany("Company Name");
+        customer.setPhone("123-456-7890");
+        customer = customerDao.saveCustomer(customer);
+
+        Item item = new Item();
+        item.setDailyRate(new BigDecimal("10.99"));
+        item.setDescription("Test");
+        item.setName("Saw");
+        item = itemDao.saveItem(item);
+
+        Invoice invoice = new Invoice();
+        invoice.setCustomerId(customer.getCustomerId());
+        invoice.setOrderDate(LocalDate.of(2019, 8, 23));
+//        invoice.setPickupDate();
     }
 
     @Test

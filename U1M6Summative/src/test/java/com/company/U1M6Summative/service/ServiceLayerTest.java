12165b51f4a60ca629ea0ee1e9851a4beae4d370
Ahmed Kaahin <ahmedkaahin@MohamedAliMacbookPro.local>
2019-09-12 13:21:01 -0400

    Pushing local changes

--- U1M6Summative/src/test/java/com/company/U1M6Summative/service/ServiceLayerTest.java
+++ U1M6Summative/src/test/java/com/company/U1M6Summative/service/ServiceLayerTest.java
@@ -1,10 +1,13 @@
 package com.company.U1M6Summative.service;
 
+import com.company.U1M6Summative.dao.CustomerDao;
 import com.company.U1M6Summative.dao.InvoiceDao;
 import com.company.U1M6Summative.dao.InvoiceItemDao;
 import com.company.U1M6Summative.dao.ItemDao;
+import com.company.U1M6Summative.dto.Customer;
 import com.company.U1M6Summative.dto.Invoice;
 import com.company.U1M6Summative.dto.InvoiceItem;
+import com.company.U1M6Summative.dto.Item;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -27,7 +30,7 @@ import static org.mockito.Mockito.mock;
 @SpringBootTest
 public class ServiceLayerTest {
         ServiceLayer service;
-        CustomerDao     customerDao;
+        CustomerDao customerDao;
         ItemDao itemDao;
         InvoiceDao invoiceDao;
         InvoiceItemDao invoiceItemDao
@@ -36,15 +39,8 @@ public class ServiceLayerTest {
         private void setUpCustomerDaoMock() {
 
 
-            custmerDao = mock (customerDaoJdbcTemplateImpl.class);
-            Custmer  customer = new Custmer();
-
-            customer_id int(11) not null auto_increment primary key,
-                    first_name varchar(50) not null,
-                    last_name varchar(50) not null,
-                    email varchar(75) not null,
-                    company varchar(50) not null,
-                    phone varchar(50) not null));
+            customerDao  = mock (customerDaoJdbcTemplateImpl.class);
+            Customer customer = new Customer();
 
             customer.setCustomerId(i);
             customer.setFirstName();
@@ -69,17 +65,15 @@ public class ServiceLayerTest {
         }
 
     private void setUpItemDaoMock() {
-
-
         ItemDao = mock (itemDaoJdbcTemplateImpl.class);
-        Item  item= new Item();
+        Item item= new Item();
         item.setItemId(1);
         item.setName("Computer");
         item.setDescription("Mack Pro");
         item.setDailyRate(new BigDecimal(1200.99));
 
 
-        Item2  item2 = new Item();
+        Item  item2 = new Item();
         item2.setItemId(1);
         item2.setName("Computer");
         item2.setDescription("Mack Pro");
@@ -94,19 +88,18 @@ public class ServiceLayerTest {
 
     }
     private void setUpInvoiceDaoMinvoiceItem{
-
-        InvoiceDao = mock (InvoiceDaoJdbcTemplateImpl.class);
+        invoiceDao = mock (InvoiceDaoImpl.class);
         Invoice  invoice = new Invoice();
         invoice.setInvoiceId(1);
         invoice.setOrderDate(LocalDate.of(2019,09,12);
-        invoice.setPickUPDate(LocalDate.of(2019,09,12);
+        invoice.setPickUpDate(LocalDate.of(2019,09,12);
         invoice.setRetunDate(LocalDate.of(2019,09,12);
         invoice.setLateFee(new BigDecimal("12.99"));
 
         Invoice invoice2= new Invoice();
         invoice.setInvoiceId(1);
         invoice.setOrderDate(LocalDate.of(2019,09,12);
-        invoice.setPickUPDate(LocalDate.of(2019,09,12);
+        invoice.setPickUpDate(LocalDate.of(2019,09,12);
         invoice.setRetunDate(LocalDate.of(2019,09,12);
         invoice.setLateFee(new BigDecimal("12.99"));
 
@@ -114,7 +107,7 @@ public class ServiceLayerTest {
         invoiceList.add(invoice);
         doReturn(invoice).when(invoiceDao).addInvoice(invoice2);
         doReturn(invoice).when(invoiceDao).getInvoice(1);
-        doReturn(invoiceList;
+        doReturn(invoiceList);
     }
 
 

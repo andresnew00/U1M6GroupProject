8e7c8aed7ef03e57572fbc1c4b61ebe2fad7ed7c
Ahmed Kaahin <ahmedkaahin@mohamedcbookpro.attlocal.net>
2019-09-13 02:12:19 -0400

    added all tests for tomorrow

--- U1M6Summative/src/test/java/com/company/U1M6Summative/service/ServiceLayerTest.java
+++ U1M6Summative/src/test/java/com/company/U1M6Summative/service/ServiceLayerTest.java
@@ -5,7 +5,10 @@ import com.company.U1M6Summative.dto.Customer;
 import com.company.U1M6Summative.dto.Invoice;
 import com.company.U1M6Summative.dto.InvoiceItem;
 import com.company.U1M6Summative.dto.Item;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,10 +17,9 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.doReturn;
-
-import static org.mockito.Mockito.mock;
-
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 
 /**
@@ -83,7 +85,7 @@ public class ServiceLayerTest {
         doReturn(itemList).when(itemDao).findAll();
 
     }
-    private void setUpInvoiceDaoMinvoiceItem(){
+    private void setUpInvoiceDao(){
         invoiceDao = mock (InvoiceDaoJdbcTemplateImp.class);
         Invoice  invoice = new Invoice();
         invoice.setInvoiceId(1);
@@ -106,13 +108,8 @@ public class ServiceLayerTest {
         doReturn(invoiceList);
     }
 
-
-
-
         private void setUpInvoiceItemDaoMock() {
-
             invoiceItemDao =  mock(InvoiceItemDaoJdbcTemplateImpl.class);
-
         InvoiceItem  invoiceItem = new InvoiceItem();
         invoiceItem.setId(1);
         invoiceItem.setInvoiceId(1);
@@ -122,16 +119,11 @@ public class ServiceLayerTest {
         invoiceItem.setDiscount(new BigDecimal("0.00"));
 
             InvoiceItem  invoiceItem2 = new InvoiceItem();
-
             invoiceItem2.setInvoiceId(1);
             invoiceItem2.setItemId(1);
             invoiceItem2.setQuantity(10);
             invoiceItem2.setUnitRate(new BigDecimal("10.99"));
             invoiceItem2.setDiscount(new BigDecimal("0.00"));
-
-
-
-
 
         List<InvoiceItem> invoiceItemList = new ArrayList<>();
         invoiceItemList.add(invoiceItem);
@@ -140,4 +132,196 @@ public class ServiceLayerTest {
         doReturn(invoiceItemList).when(invoiceItemDao).getAllInvoiceItems();
    }
 
+    @Before
+    public void setUp() throws Exception {
+        setUpCustomerDaoMock();
+        setUpItemDaoMock();
+        setUpInvoiceItemDaoMock();
+        setUpInvoiceDaoMinvoiceItem();
+        service = new ServiceLayer(customerDao, invoiceDao, invoiceItemDao,itemDao);
+    }
+
+    @Test
+    public void saveCustomer() {
+        Customer customer = new Customer();
+        customer.setLastName("Jay");
+        customer.setEmail("@jay");
+        customer.setCompany("JayComp");
+        customer.setPhone("111-222-3333");
+        customer = service.saveCustomer(customer);
+    }
+    @Test
+    public void findCustomer(){
+        Customer customer = new Customer();
+        customer.setLastName("Jay");
+        customer.setEmail("@jay");
+        customer.setCompany("JayComp");
+        customer.setPhone("111-222-3333");
+        Customer fromService = service.findCustomer(customer.getCustomerId());
+        assertEquals(customer, fromService);
+    }
+    @Test
+    public void findAllCustomers(){
+        Customer customer = new Customer();
+        customer.setLastName("Jay");
+        customer.setEmail("@jay");
+        customer.setCompany("JayComp");
+        customer.setPhone("111-222-3333");
+        List<Customer> customerList = service.findAllCustomers();
+        assertEquals(1, customerList.size());
+        assertEquals(customer, customerList.get(0));
+    }
+    @Test
+    public void updateCustomer() {
+        Customer customer = new Customer();
+        customer.setLastName("Jay");
+        customer.setEmail("@jay");
+        customer.setCompany("JayComp");
+        customer.setPhone("111-222-3333");
+        customer = service.saveCustomer(customer);
+        ArgumentCaptor<Customer> customerCaptor = ArgumentCaptor.forClass(Customer.class);
+        doNothing().when(customerDao).updateCustomer(customerCaptor.capture());
+        service.updateCustomer(customer);
+        verify(customerDao,times(1)).updateCustomer(customerCaptor.capture().getValue());
+        Customer customer2 = customerCaptor.getValue();
+        assertEquals(customer ,customer2);
+    }
+    @Test
+    public void deleteCustomer() {
+        Customer customer = new Customer();
+        customer.setCustomerId(10);
+        ArgumentCaptor<Integer> integerCaptor = ArgumentCaptor.forClass(Integer.class);
+        doNothing().when(customerDao).deleteCustomer(integerCaptor.capture());
+        service.removeCustomer(10);
+        verify(customerDao, times(1)).deleteCustomer(integerCaptor.getValue());
+        assertEquals(10, integerCaptor.getValue().intValue());
+    }
+
+    //=======================================================
+
+    @Test
+    public void saveItem() {
+        Item item = new Item();
+        item.setItemId(1);
+        item.setName("Computer");
+        item.setDescription("Mack Pro");
+        item.setDailyRate(new BigDecimal(1200.99));
+        item = service.saveItem(item);
+    }
+    @Test
+    public void findItem(){
+        Item item = new Item();
+        item.setItemId(1);
+        item.setName("Computer");
+        item.setDescription("Mack Pro");
+        item.setDailyRate(new BigDecimal(1200.99));
+        item fromService = service.findItem(item.getItemId());
+        assertEquals(item, fromService);
+    }
+    @Test
+    public void findAllItem(){
+        Item item = new Item();
+        item.setItemId(1);
+        item.setName("Computer");
+        item.setDescription("Mack Pro");
+        item.setDailyRate(new BigDecimal(1200.99));
+        List<Item> itemList = service.findAllItems();
+        assertEquals(1, itemList.size());
+        assertEquals(item, itemList.get(0));
+    }
+    @Test
+    public void updateItem() {
+        Item item = new Item();
+        item.setItemId(1);
+        item.setName("Computer");
+        item.setDescription("Mack Pro");
+        item.setDailyRate(new BigDecimal(1200.99));
+        ArgumentCaptor<Item> itemCaptor = ArgumentCaptor.forClass(Item.class);
+        doNothing().when(invoiceDao).updateCustomer(itemCaptor.capture());
+        service.updateItem(item);
+        verify(itemDao,times(1)).updateItem(itemCaptor.capture().getValue());
+        Item item2 = itemCaptor.getValue();
+        assertEquals(item, item2);
+    }
+    @Test
+    public void deleteItem(){
+        Item item = new Item();
+        item.setItemId(1);
+        item.setName("Computer");
+        item.setDescription("Mack Pro");
+        item.setDailyRate(new BigDecimal(1200.99));
+        ArgumentCaptor<Integer> integerCaptor = ArgumentCaptor.forClass(Integer.class);
+        doNothing().when(itemDao).deleteItem(integerCaptor.capture());
+        service.removeItem(10);
+        verify(itemDao, times(1)).deleteItem(integerCaptor.getValue());
+        item.setItemId(10);
+        assertEquals(10, integerCaptor.getValue().intValue());
+    }
+
+
 }
+
+    @Test
+    public void findInvoiceByCustomer() {
+        Invoice invoice = new Invoice();
+        invoice.setInvoiceId(1);
+        invoice.setCustomerId(10);
+        invoice.setOrderDate(LocalDate.of(2019, 9, 12));
+        invoice.setPickupDate(LocalDate.of(2019, 9, 12));
+        invoice.setReturnDate(LocalDate.of(2019, 9, 12));
+        invoice.setLateFee(new BigDecimal("12.99"));
+        service.addInvoice(invoice);
+
+        Invoice invoice2 = new Invoice();
+        invoice2.setInvoiceId(1);
+        invoice2.setCustomerId(10);
+        invoice2.setOrderDate(LocalDate.of(2019, 9, 12));
+        invoice2.setPickupDate(LocalDate.of(2019, 9, 12));
+        invoice2.setReturnDate(LocalDate.of(2019, 9, 12));
+        invoice2.setLateFee(new BigDecimal("12.99"));
+        service.addInvoice(invoice);
+
+
+        List<Invoice> invoiceList = service.geInvoiceByCustomerId(10);
+        assertEquals(invoiceList.size(), 2);
+
+        invoiceList = service.getInvoiceByCustomerId(10);
+        assertEquals(invoiceList.size(), 1);
+    }
+
+    @Test
+    public void saveAndDeleteInvoiceByItem() {
+
+        InvoiceItem  invoiceItem = new InvoiceItem();
+        invoiceItem.setId(1);
+        invoiceItem.setInvoiceId(1);
+        invoiceItem.setItemId(1);
+        invoiceItem.setQuantity(10);
+        invoiceItem.setUnitRate(new BigDecimal("10.99"));
+        invoiceItem.setDiscount(new BigDecimal("0.00"));
+        invoiceItem.setDailyRate(new BigDecimal(1200.99));
+        invoiceItem = service.saveItem(invoiceItem);
+
+        ArgumentCaptor<Integer> integerCaptor = ArgumentCaptor.forClass(Integer.class);
+        doNothing().when(invoiceItemDao).deleteInvoiceItem(integerCaptor.capture());
+        service.removeItem(10);
+        verify(itemDao, times(1)).deleteItem(integerCaptor.getValue());
+        invoiceItem.setItemId(10);
+        assertEquals(10, integerCaptor.getValue().intValue();
+    }
+
+}
+
+
+
+
+
+
+
+
+// *Your REST API must allow the end user to:
+//        * Perform standard CRUD operations for Customers
+//        * Perform standard CRUD operations for Items
+//        * Create and delete Invoices, including the associated
+//           Invoice Items
+//        * Find Invoices by Customer
